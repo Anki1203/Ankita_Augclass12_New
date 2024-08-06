89320a652427088ca685725bd9178e71e70c58b9
aacha <aacha@LAPTOP-64QP7HP3.home>
2024-08-06 22:19:27 +0200

    First time code push

---
+++ Project July 27/src/Newjuly/Day27july.java
@@ -0,0 +1,27 @@
+package Newjuly;
+
+public class Day27july {
+		
+			int rollno;
+			int age;
+			public void display1() {
+				System.out.println("Welcome to all of you");
+			}
+			public void display2() {
+				System.out.println("Automation is very easy");
+			}
+			public static void main(String[] args) 
+			{				
+				Day27july ankita=new Day27july();
+				ankita.rollno=100;
+				ankita.age=32;	
+				ankita.display1();
+				ankita.display2();
+				System.out.println("ankita.rollno" + ankita.rollno );
+				System.out.println("ankita.age" + ankita.age);
+				
+			}
+				
+}
+
+
